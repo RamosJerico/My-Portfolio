2fed8b79f6c0b6bc497c94f6b64b1598730c2697
RamosJerico <ramosricojames26@gmail.com>
2022-12-18 14:14:33 +0800

    Updates

---
+++ precache-manifest.5a08831713e56c1d34efe384bdee2ce0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378f7abcc37587ade7b2584cc8d0b64e",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "36629c0d3d651bd205e6",
+    "url": "/My-Portfolio/static/css/main.2a8d784a.chunk.css"
+  },
+  {
+    "revision": "5ff54f0edb687742434b",
+    "url": "/My-Portfolio/static/js/2.1fc88b09.chunk.js"
+  },
+  {
+    "revision": "43da27bf8d85ddf76657c94d63edf227",
+    "url": "/My-Portfolio/static/js/2.1fc88b09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36629c0d3d651bd205e6",
+    "url": "/My-Portfolio/static/js/main.8ed25e1d.chunk.js"
+  },
+  {
+    "revision": "d0018051a852fcf640da",
+    "url": "/My-Portfolio/static/js/runtime-main.2feeaf4e.js"
+  },
+  {
+    "revision": "425ae0f7a5b18bbee7e364b50b47d050",
+    "url": "/My-Portfolio/static/media/prgbackground.425ae0f7.jpg"
+  }
+]);
